16eb7aef5d57301d6418ce1602882716bae6fb27
victor-coppin <victor.coppin@edu.dsti.institute>
2025-02-20 09:48:13 +0100

    Test env

--- OrderBook_main.py
+++ OrderBook_main.py
@@ -1,14 +1,15 @@
-# Main programm to run the order book simulation
-'''
+# Main program to run the order book simulation
+"""
 Created on 19.02.2025
-Author: Victor Coppin
+Author: Victor COPPIN
 
-Main_commands : 
+Main_commands :
 0 - Quit
 1 - Add order
 2 - list finished tasks
 3 - list unfinished tasks
 4 - mark task as finished
 5 - programmers
-6 - status of progammer
-'''
+6 - status of programmer
+"""
+print('Hello World')
