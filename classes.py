52a71a12dda32e43794b7f9a3677f29da0189bc8
victor-coppin <victor.coppin@edu.dsti.institute>
2025-03-14 16:55:35 +0100

    add comment and future implementation to be do

--- classes.py
+++ classes.py
@@ -12,7 +12,7 @@ Should have the following attributes:
 """
 
 class Task:
-    id_counter = 0
+    id_counter = 0 # class attribute that will be used to create a unique ID
     __slots__ = ['description','workload','programmer','__done_status','id'] #avoid dynamic creation of attributes
 
     def __init__(self,task_description,estimated_hours_for_completion,programmer_assigned_name):
@@ -20,8 +20,17 @@ class Task:
         self.workload = estimated_hours_for_completion
         self.programmer = programmer_assigned_name
 
-        type(self).id_counter += 1
+        type(self).id_counter += 1 # incrementation of the id_counter
         self.__done_status = "NOT_FINISHED"
         self.id = type(self).id_counter
+
+"""
+Change the behavior of the id by made it private with a get method 
+be carefully to avoid decrease of the id_counter : if one task is complete, counter //
+shouldn't decrease --> if a task is complete the function to count the number of tasks //
+should count the number of tasks with the status NOT_FINISHED
+"""
+
+
 
 
