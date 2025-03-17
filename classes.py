82bbf03cadefcbbfb1eacb934a927e2013de42eb
victor-coppin <victor.coppin@edu.dsti.institute>
2025-03-17 21:20:20 +0100

    add is_finished getter as boolean and mark_finish method

--- classes.py
+++ classes.py
@@ -15,14 +15,18 @@ class Task:
     __id_counter = 0 # class attribute that will be used to create a unique ID
     __slots__ = ['description','workload','programmer','__done_status','id'] #avoid dynamic creation of attributes
 
-    def __init__(self,task_description,estimated_hours_for_completion,programmer_assigned_name):
+    def __init__(self,task_description,programmer_assigned_name,estimated_hours_for_completion):
         self.description = task_description
         self.workload = estimated_hours_for_completion
         self.programmer = programmer_assigned_name
-
         type(self).__id_counter += 1 # incrementation of the id_counter
-        self.__done_status = "NOT_FINISHED"
-        self.id = type(self).__id_counter #type(self) is used in case class name is changed : avoid task.id
+        self.id = type(self).__id_counter #type(self) is used in case class name is changed:i.e. avoid task.id
+        self.__done_status = "Not_Finished"
+    @property
+    def is_finished(self): #getter that return a boolean by compare the __done_status's value
+        return  self.__done_status == "Finished"
+
+
     """
     Methods
     """
@@ -30,7 +34,8 @@ class Task:
     def number_of_tasks(cls):
         return cls.__id_counter
 
-
+    def mark_finished(self):
+        self.__done_status = "Finished"
 
     def __str__(self):
         return str(self.id) + ": " + self.description + " (" + str(self.workload) + "hours)," + " programmer " + self.programmer
@@ -40,10 +45,12 @@ be carefully to avoid decrease of the id_counter : if one task is complete, coun
 shouldn't decrease --> if a task is complete the function to count the number of tasks //
 should count the number of tasks with the status NOT_FINISHED
 """
-x = Task("program hello world",3,"Eric")
+x = Task("program hello world","Eric",3)
 print(x)
 print(Task.number_of_tasks())
 print(x.number_of_tasks())
-
-
+print(x.is_finished)
+x.mark_finished()
+print(x.is_finished)
+print(x.id)
 
