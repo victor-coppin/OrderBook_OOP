69bd3116e394bce028686ffac06f518fcf37c413
victor-coppin <victor.coppin@edu.dsti.institute>
2025-03-28 21:48:11 +0100

    add dictionary for programmer and method programmer that return the keys and test for 2 part

--- Order_Book/classes.py
+++ Order_Book/classes.py
@@ -26,7 +26,7 @@ class Task:
         self.__programmer = programmer
 
         type(self).__id_generator += 1 # incrementation of the id_generator
-        self.__id = type(self).__id_generator #type(self) is used in case class name is changed:i.e. avoid Task.id
+        self.__id_number = type(self).__id_generator #type(self) is used in case class name is changed:i.e. avoid Task.id
         self.__done_status = "NOT FINISHED" # think if better to stock as a boolean
 
     @property #encapsulation
@@ -41,8 +41,8 @@ class Task:
     def workload(self):
         return self.__workload
     @property
-    def id(self):
-        return self.__id
+    def id_number(self):
+        return self.__id_number
 
     """
     Class Methods
@@ -60,43 +60,39 @@ class Task:
         self.__done_status = "FINISHED"
 
     def __str__(self):
-        return (str(self.id) + ": " + self.description + " (" + str(self.workload) + " hours),"
+        return (str(self.__id_number) + ": " + self.description + " (" + str(self.workload) + " hours),"
                 + " programmer " + self.programmer +" "+self.__done_status)
-
 """
 Class OrderBook
 The orderbook that used to store, add and query tasks 
 """
-
-#  [MermaidChart: fc25ec32-0aab-42e4-811c-b68f888f0d09]
 class OrderBook(Task):
     def __init__(self):
         super().__init__(description="add description", programmer="add your name",workload=0)
-        self.order_dictionary = {}
+        self.order_dictionary = {} #contains all the order's instances with the order.id as a key
+        self.order_dictionary_programmer = {} #contains a tuple that represent tasks for programmers as key
     def add_order(self, description, programmer, workload):
         order = Task(description, programmer, workload)
-        self.order_dictionary[(order.id,order.programmer)] = order
+        self.order_dictionary[order.id_number] = order
+        self.order_dictionary_programmer[programmer] = []
+        self.order_dictionary_programmer[order.programmer].append((order.id_number,order.description, order.workload))
         return order
 
+    def __str__(self,programmer_orders = False):
+        return super().__str__()
     def all_orders(self):
         return self.order_dictionary.values()
+
+    def programmer(self):
+        return self.order_dictionary_programmer.keys()
+    # def programmer_task(self):
+    #         str()self.order_dictionary_programmer.keys()
 
 orders = OrderBook()
 orders.add_order("program webstore", "Adele", 10)
 orders.add_order("program mobile app for workload accounting", "Eric", 25)
 orders.add_order("program app for practising mathematics", "Adele", 100)
-for order in orders.all_orders():
-    print(order)
-print(orders.order_dictionary)
-
-
-
-
-
-
-
-
-
-
+for programmer in orders.programmer():
+    print(programmer)
 
 
--- Test/test_orderbook.py
+++ Test/test_orderbook.py
@@ -26,14 +26,14 @@ def test_task_print_attributes(capsys,monkeypatch):
     assert t1.is_finished() is not True
     """
     t1 = Task("program hello world","Eric",3) #create first instance part 1
-    monkeypatch.setattr(t1,"_Task__id", 1)
-    print(t1.id,t1.description,t1.programmer,t1.workload)
-    capture_print_output = capsys.readouterr()
-    assert capture_print_output.out == str(t1.id) + " program hello world Eric 3\n" #print auto create \n newline
+    monkeypatch.setattr(t1,"_Task__id_number", 1)
+    print(t1.id_number,t1.description,t1.programmer,t1.workload)
+    output = capsys.readouterr() #get the output of the print above
+    assert output.out == str(t1.id_number) + " program hello world Eric 3\n" #print auto create \n newline
 
 def test_print_task(monkeypatch):
     t1 = Task("program hello world", "Eric", 3)
-    monkeypatch.setattr(t1,"_Task__id",1)
+    monkeypatch.setattr(t1,"_Task__id_number",1)
     monkeypatch.setattr(t1,"_Task__done_status","NOT FINISHED")
     assert str(t1) == "1: program hello world (3 hours), programmer Eric NOT FINISHED"
 
@@ -47,22 +47,30 @@ def test_print_is_not_finished():
     t1.mark_finished()
     assert t1.is_finished() is True
 
-# def test_order_book(monkeypatch):
-#     orders = OrderBook()
-#     orders.add_order("program webstore", "Adele", 10)
-#     orders.add_order("program mobile app for workload accounting", "Eric", 25)
-#     orders.add_order("program app for practising mathematics", "Adele", 100)
-#     for order in orders.all_orders():
-#          print(order)
-#     assert orders.order_dictionary == {
-#         (1, 'Adele'): [
-#             ("program webstore", 10),
-#             ("program app for practising mathematics", 100)
-#         ],
-#         "Eric": [
-#             ("program mobile app for workload accounting", 25)
-#         ]
-#     }
+def test_order_book_print_all_order(capsys,monkeypatch):
+    orders = OrderBook()
+    t1 = orders.add_order("program webstore", "Adele", 10)
+    monkeypatch.setattr(t1, "_Task__id_number", 1)
+    t2 = orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    monkeypatch.setattr(t2, "_Task__id_number", 2)
+    t3 = orders.add_order("program app for practising mathematics", "Adele", 100)
+    monkeypatch.setattr(t3, "_Task__id_number", 3)
+    for order in orders.all_orders():
+        print(order)
+    capture_print_output = capsys.readouterr()
+    assert (capture_print_output.out ==
+        ("1: program webstore (10 hours), programmer Adele NOT FINISHED\n"
+        "2: program mobile app for workload accounting (25 hours), programmer Eric NOT FINISHED\n"
+        "3: program app for practising mathematics (100 hours), programmer Adele NOT FINISHED\n"))
 
-
-
+def test_order_book_print_all_unique_programmer(capsys,monkeypatch):
+    orders = OrderBook()
+    orders.add_order("program webstore", "Adele", 10)
+    orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    orders.add_order("program app for practising mathematics", "Adele", 100)
+    for programmer in orders.programmer():
+        print(programmer)
+    capture_print_output = capsys.readouterr()
+    assert (capture_print_output.out ==
+        ("Adele\n"
+         "Eric\n"))
